1112d08a5a3c78875bc83c9cf9a1c75a4d2f803d
baiyan564815618 <2807940472@qq.com>
2026-05-19 09:13:50 +0800

    fix mysql none error

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -76,33 +76,18 @@ TEMPLATES = [
 WSGI_APPLICATION = 'blog_project.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/6.0/ref/settings/#databases
-if os.environ.get('MYSQLHOST'):
-    # Railway 生产环境配置
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.mysql',
-            'NAME': os.environ.get('MYSQLDATABASE'),
-            'USER': os.environ.get('MYSQLUSER'),
-            'PASSWORD': os.environ.get('MYSQLPASSWORD'),
-            'HOST': os.environ.get('MYSQLHOST'),
-            'PORT': os.environ.get('MYSQLPORT'),
-        }
+# 优先读取 Railway 自动注入的变量，不存在则用本地配置
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.environ.get('MYSQLDATABASE', 'blog'),
+        'USER': os.environ.get('MYSQLUSER', 'root'),
+        'PASSWORD': os.environ.get('MYSQLPASSWORD', 'root'),
+        'HOST': os.environ.get('MYSQLHOST', '127.0.0.1'),
+        'PORT': os.environ.get('MYSQLPORT', '3307'),
     }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.mysql',
-            'NAME': 'blog',
-            'USER': 'root',
-            'PASSWORD': 'root',
-            'HOST': '127.0.0.1',
-            'PORT': '3307',
-        }
-    }
+}
 
-# 在 settings.py 里添加
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Password validation
